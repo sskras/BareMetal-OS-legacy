8b6ad037726aac7c02c8082f48b2633d326a4eec
Saulius Krasuckas <saulius.krasuckas@samesystem.com>
2022-11-05 14:08:55 +0200

    os/{sysvar,drivers/pci,drivers/storage/ahci}.asm: Minimize AHCI to IDing and reading

--- os/drivers/pci.asm
+++ os/drivers/pci.asm
@@ -6,9 +6,6 @@
 ; =============================================================================
 
 align 16
-db 'DEBUG: PCI      '
-align 16
-
 
 ; -----------------------------------------------------------------------------
 ; os_pci_read_reg -- Read from a register on a PCI device
@@ -39,40 +36,6 @@ os_pci_read_reg:
 	pop rcx
 	pop rdx
 ret
-; -----------------------------------------------------------------------------
-
-
-; -----------------------------------------------------------------------------
-; os_pci_write_reg -- Write to a register on a PCI device
-;  IN:	BL  = Bus number
-;	CL  = Device/Slot/Function number
-;	DL  = Register number (0-15)
-; OUT:	EAX = Register information
-;	All other registers preserved
-os_pci_write_reg:
-	push rdx
-	push rcx
-	push rbx
-	push rax
-
-	shl ebx, 16			; Move Bus number to bits 23 - 16
-	shl ecx, 8			; Move Device/Slot/Fuction number to bits 15 - 8
-	mov bx, cx
-	shl edx, 2
-	mov bl, dl
-	and ebx, 0x00ffffff		; Clear bits 31 - 24
-	or ebx, 0x80000000		; Set bit 31
-	mov eax, ebx
-	mov dx, PCI_CONFIG_ADDRESS
-	out dx, eax
-	pop rax
-	mov dx, PCI_CONFIG_DATA
-	out dx, eax
-
-	pop rbx
-	pop rcx
-	pop rdx
-	ret
 ; -----------------------------------------------------------------------------
 
 
--- os/drivers/storage/ahci.asm
+++ os/drivers/storage/ahci.asm
@@ -1,19 +1,17 @@
 ; =============================================================================
-; BareMetal -- a 64-bit OS written in Assembly for x86-64 systems
+;;; BareMetal -- a 64-bit OS written in Assembly for x86-64 systems
 ; Copyright (C) 2008-2016 Return Infinity -- see LICENSE.TXT
 ;
 ; AHCI Driver
 ; =============================================================================
 
 align 16
-db 'DEBUG: AHCI     '
-align 16
 
 
 ; -----------------------------------------------------------------------------
 init_ahci:
-	mov rsi, diskmsg
-	call os_output
+;;	mov rsi, diskmsg
+;;	call os_output
 
 ; Probe for an AHCI hard drive controller
 	xor ebx, ebx			; Clear the Bus number
@@ -101,19 +99,19 @@ founddrive:
 
 	; Query drive
 	pop rcx				; Restore port number
-	mov rdi, os_temp
-	mov rsi, rdi
-	call iddrive
-	mov eax, [rsi+200]		; Max LBA Extended
-	shr rax, 11			; rax = rax * 512 / 1048576	MiB
+;;	mov rdi, os_temp
+;;	mov rsi, rdi
+;;	call iddrive
+;;	mov eax, [rsi+200]		; Max LBA Extended
+;;	shr rax, 11			; rax = rax * 512 / 1048576	MiB
 ;	shr rax, 21			; rax = rax * 512 / 1073741824	GiB
-	mov [hd1_size], eax		; in mebibytes (MiB)
-	mov rdi, os_temp_string
-	mov rsi, rdi
-	call os_int_to_string
-	call os_output
-	mov rsi, mibmsg
-	call os_output
+;;	mov [hd1_size], eax		; in mebibytes (MiB)
+;;	mov rdi, os_temp_string
+;;	mov rsi, rdi
+;;	call os_int_to_string
+;;	call os_output
+;;	mov rsi, mibmsg
+;;	call os_output
 
 	; Found a bootable drive
 	mov byte [os_DiskEnabled], 0x01
@@ -122,88 +120,9 @@ founddrive:
 
 init_ahci_err_noahci:
 hdd_setup_err_nodisk:
-	mov rsi, namsg
-	call os_output
+;;	mov rsi, namsg
+;;	call os_output
 
-	ret
-; -----------------------------------------------------------------------------
-
-
-; -----------------------------------------------------------------------------
-; iddrive -- Identify a SATA drive
-; IN:	RCX = Port # to query
-;	RDI = memory location to store details (512 bytes)
-; OUT:	Nothing, all registers preserved
-iddrive:
-	push rdi
-	push rsi
-	push rcx
-	push rax
-	push rdi			; Save the destination memory address
-
-	mov rsi, [ahci_base]
-	shl rcx, 7			; Quick multiply by 0x80
-	add rcx, 0x100			; Offset to port 0
-	add rsi, rcx
-
-	; Build the Command List Header
-	mov rdi, ahci_cmdlist		; command list (1K with 32 entries, 32 bytes each)
-	mov eax, 0x00010005		; 1 PRDTL Entry, Command FIS Length = 20 bytes
-	stosd				; DW 0 - Description Information
-	xor eax, eax
-	stosd				; DW 1 - Command Status
-	mov rax, ahci_cmdtable
-	stosd				; DW 2 - Command Table Base Address
-	shr rax, 32			; 63..32 bits of address
-	stosd				; DW 3 - Command Table Base Address Upper
-	xor eax, eax
-	stosq				; DW 4-7 are reserved
-	stosq
-
-	; Build the Command Table
-	mov rdi, ahci_cmdtable		; Build a command table for Port 0
-	mov eax, 0x00EC8027		; EC identify, bit 15 set, fis 27 H2D
-	stosd				; feature 7:0, command, c, fis
-	xor eax, eax
-	stosq				; the rest of the table can be clear
-	stosq
-
-	; PRDT - pysical region descriptor table
-	mov rdi, ahci_cmdtable + 0x80
-	pop rax				; Restore the destination memory address
-	stosd				; Data Base Address
-	shr rax, 32
-	stosd				; Data Base Address Upper
-	xor eax, eax
-	stosd				; Reserved
-	mov eax, 0x000001FF		; 512 - 1
-	stosd				; Description Information
-
-	xor eax, eax
-	mov [rsi+AHCI_PxIS], eax	; Port x Interrupt Status
-
-	mov eax, 0x00000001		; Execute Command Slot 0
-	mov [rsi+AHCI_PxCI], eax
-
-	xor eax, eax
-	bts eax, 4			; FIS Recieve Enable (FRE)
-	bts eax, 0			; Start (ST)
-	mov [rsi+AHCI_PxCMD], eax	; Offset to port 0 Command and Status
-
-iddrive_poll:
-	mov eax, [rsi+AHCI_PxCI]	; Read Command Slot 0 status
-	test eax, eax
-	jnz iddrive_poll
-
-	mov eax, [rsi+AHCI_PxCMD]	; Offset to port 0
-	btr eax, 4			; FIS Receive Enable (FRE)
-	btr eax, 0			; Start (ST)
-	mov [rsi+AHCI_PxCMD], eax
-
-	pop rax
-	pop rcx
-	pop rsi
-	pop rdi
 	ret
 ; -----------------------------------------------------------------------------
 
@@ -329,127 +248,6 @@ readsectors_error:
 	ret
 ; -----------------------------------------------------------------------------
 
-
-; -----------------------------------------------------------------------------
-; writesectors -- Write data to a SATA hard drive
-; IN:	RAX = starting sector # to write (48-bit LBA Address)
-;	RCX = number of sectors to write (up to 8192 = 4MiB)
-;	RDX = disk #
-;	RSI = memory location of sectors
-; OUT:	RAX = RAX + number of sectors that were written
-;	RCX = number of sectors that were written (0 on error)
-;	RSI = RSI + (number of sectors written * 512)
-;	All other registers preserved
-writesectors:
-	push rdx
-	push rbx
-	push rdi
-	push rsi
-	push rcx
-	push rax
-
-	mov rbx, 0xFFFFFFFFFFFF		; Check for invalid starting sector
-	cmp rax, rbx
-	jg writesectors_error
-
-	push rcx			; Save the sector count
-	push rsi			; Save the source memory address
-	push rax			; Save the block number
-	push rax
-
-	mov rsi, [ahci_base]
-	shl rdx, 7			; Quick multiply by 0x80
-	add rdx, 0x100			; Offset to port 0
-	add rsi, rdx
-
-	; Build the Command List Header
-	mov rdi, ahci_cmdlist		; command list (1K with 32 entries, 32 bytes each)
-	xor eax, eax
-	mov eax, 0x00010045		; 1 PRDTL Entry, write flag (bit 6), Command FIS Length = 20 bytes
-	stosd				; DW 0 - Description Information
-	xor eax, eax
-	stosd				; DW 1 - Command Status
-	mov rax, ahci_cmdtable
-	stosd				; DW 2 - Command Table Base Address
-	shr rax, 32			; 63..32 bits of address
-	stosd				; DW 3 - Command Table Base Address Upper
-	xor eax, eax
-	stosq				; DW 4 - 7 are reserved
-	stosq
-
-	; Build the Command Table
-	mov rdi, ahci_cmdtable		; Build a command table for Port 0
-	mov eax, 0x00358027		; 35 WRITE DMA EXT, bit 15 set, fis 27 H2D
-	stosd				; feature 7:0, command, c, fis
-	pop rax				; Restore the start sector number
-	shl rax, 36
-	shr rax, 36			; Upper 36 bits cleared
-	bts rax, 30			; bit 30 set for LBA
-	stosd				; device, lba 23:16, lba 15:8, lba 7:0
-	pop rax				; Restore the start sector number
-	shr rax, 24
-	stosd				; feature 15:8, lba 47:40, lba 39:32, lba 31:24
-	mov rax, rcx			; Read the number of sectors given in rcx
-	stosd				; control, ICC, count 15:8, count 7:0
-	xor eax, eax
-	stosd				; reserved
-
-	; PRDT setup
-	mov rdi, ahci_cmdtable + 0x80
-	pop rax				; Restore the source memory address
-	stosd				; Data Base Address
-	shr rax, 32
-	stosd				; Data Base Address Upper
-	xor eax, eax
-	stosd				; Reserved
-	pop rax				; Restore the sector count
-	shl rax, 9			; multiply by 512 for bytes
-	dec rax				; subtract 1 (4.2.3.3, DBC is number of bytes - 1)
-	stosd				; Description Information
-
-	xor eax, eax
-	mov [rsi+AHCI_PxIS], eax	; Port x Interrupt Status
-
-	mov eax, 0x00000001		; Execute Command Slot 0
-	mov [rsi+AHCI_PxCI], eax
-
-	xor eax, eax
-	bts eax, 4			; FIS Recieve Enable (FRE)
-	bts eax, 0			; Start (ST)
-	mov [rsi+AHCI_PxCMD], eax	; Offset to port 0 Command and Status
-
-writesectors_poll:
-	mov eax, [rsi+AHCI_PxCI]
-	test eax, eax
-	jnz writesectors_poll
-
-	mov eax, [rsi+AHCI_PxCMD]	; Offset to port 0
-	btr eax, 4			; FIS Receive Enable (FRE)
-	btr eax, 0			; Start (ST)
-	mov [rsi+AHCI_PxCMD], eax
-
-	pop rax				; rax = start
-	pop rcx				; rcx = number of sectors read
-	add rax, rcx			; rax = start + number of sectors written
-	pop rsi
-	pop rdi
-	mov rbx, rcx			; rdi = dest addr + number of bytes written
-	shl rbx, 9
-	add rdi, rbx
-	pop rbx
-	pop rdx
-	ret
-
-writesectors_error:
-	pop rax
-	pop rcx
-	pop rsi
-	pop rdi
-	pop rbx
-	pop rdx
-	xor ecx, ecx
-	ret
-; -----------------------------------------------------------------------------
 
 
 ; HBA Memory Registers
